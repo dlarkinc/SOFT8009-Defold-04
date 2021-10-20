a18c0ea04bed1edc6a1314521d8276cf146192d8
Larkin Cunningham <larkin.cunningham@cit.ie>
2021-10-20 12:06:32 +0100

    added game save data and json parse/stringify

---
+++ lib/game_data.lua
@@ -0,0 +1,15 @@
+json = require "lib.myjson"
+
+function load_game_data()
+	file = io.open("data/game.json", "r")
+	json_string = file:read("*a")
+	file:close()
+	table_data = json.parse(json_string)
+	return table_data
+end
+
+function save_game_data(game_data)
+	file = io.open("data/game.json", "w")
+	file:write(json.stringify(game_data))
+	file:close()
+end
